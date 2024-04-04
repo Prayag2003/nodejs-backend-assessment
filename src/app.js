6b721413336d76161eeb9b8fd88725c8ea933960
Prayag <prayagbhatt2003@gmail.com>
2024-04-04 10:24:22 +0530

    setup morgan for logging

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require('morgan');
+const responseTime = require('response-time');
+const chalk = require('chalk');
 
 const app = express()
-app.use(morgan("dev"))
+
+app.use(responseTime());
+morgan.token('table', (req, res) => {
+    const method = req.method;
+    const url = req.originalUrl;
+    const status = res.statusCode;
+    const responseTime = res.getHeaders()['x-response-time'];
+    const remoteAddress = req.ip;
+
+    return `METHOD: ${chalk.green(method)} | URL: ${chalk.blue(url)} | Status: ${chalk.yellow(status.toString())} | Response Time: ${chalk.magenta(responseTime)} | IP: ${chalk.cyan(remoteAddress)}`;
+});
+
+
+app.use(morgan(':table'));
 
 app.use(bodyParser.json());
 app.use(
